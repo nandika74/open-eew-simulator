9fbc08e3e2554673ad405c1625a3922f7a5f7a6d
Nandika L <nandika.liyanage@yahoo.com>
2021-06-25 14:32:30 +0200

    Open EEW Simulator initial codebase

---
+++ src/main/java/edu/massey/eew/simulator/openeewsimulator/OpenEewSimulatorApplication.java
@@ -0,0 +1,13 @@
+package edu.massey.eew.simulator.openeewsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenEewSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenEewSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/massey/eew/simulator/openeewsimulator/OpenEewSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.massey.eew.simulator.openeewsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenEewSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
